916982ccbc503a933a5051a15915fddc85e6ab6d
Jacobeee <jacobtasticyt@gmail.com>
2026-04-15 10:13:19 -0400

    Added part of project outline
    - added a description of features that can be added for this project, such as player interactions, food algorithm, and the basic map idea

--- main.asm
+++ main.asm
@@ -1,3 +1,14 @@
+; Survival Game
+; - player spawns
+	;- controlled with WASD
+	;- can pick up with E
+	;- consume with F
+; food spawns randomly
+	;- to pick up food, you have to be in a specific radius to pick up with E
+; display health/hunger bar
+; map can be the window thing we seen in class (the thing where only a portion of the map is visible at a time)
+; maybe enemies added on later
+
 INCLUDE Irvine32.inc
 
 .data
